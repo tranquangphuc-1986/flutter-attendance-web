c49adf8797ac81439597da41b7319fc555aae6a4
tranquangphuc-1986 <phucthanh1572013@gmail.com>
2025-07-22 18:31:24 +0700

    update code

--- lib/student_screens/students_statistics_page.dart
+++ lib/student_screens/students_statistics_page.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+import 'dart:html' as htmt;
 import 'dart:io';
 import 'package:app_02/models/student.dart';
-import 'package:app_02/student_screens/StatisticsExcel.dart';
 import 'package:excel/excel.dart' as excel;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -69,57 +71,85 @@ class _StudentsStatisticsPageState extends State<StudentsStatisticsPage> {
     }
   }
 
-//Hàm xuất Excel
-   Future<void> exportToExcel(BuildContext context, List<Map<String, dynamic>> studentsData) async {
+  //Hàm xuất Excel
+  Future<void> exportToExcel(
+    BuildContext context,
+    List<Map<String, dynamic>> studentsData,
+  ) async {
     try {
-    // Tạo workbook Excel
-    final ex= excel.Excel.createExcel();
-    final excel.Sheet sheet = ex['Tổng hợp điểm danh'];
+      // Tạo workbook Excel
+      final ex = excel.Excel.createExcel();
+      final excel.Sheet sheet = ex['Tổng hợp điểm danh'];
 
-    // Tiêu đề
-    sheet.appendRow([
-      'Họ và tên',
-      'Đơn vị',
-      'Có mặt',
-      'Công tác',
-      'Bị ốm',
-      'Nghỉ phép',
-      'Việc riêng',
-      'Đi học',
-      'Đi trễ',
-      'Không lý do',
-      'Tổng số vắng'
-    ]);
+      // Tiêu đề
+      sheet.appendRow([
+        'Họ và tên',
+        'Đơn vị',
+        'Có mặt',
+        'Công tác',
+        'Bị ốm',
+        'Nghỉ phép',
+        'Việc riêng',
+        'Đi học',
+        'Đi trễ',
+        'Không lý do',
+        'Tổng số vắng',
+      ]);
 
-    // Ghi dữ liệu từng cán bộ
-    for (var student in studentsData) {
-      final String name = student['name'] ?? '';
-      final String className = student['className'] ?? '';
-      final int present =student['present'] ?? 0;
-      final int work = student['work'] ?? 0;
-     final int sick = student['sick'] ?? 0;
-     final int np = student['np'] ?? 0;
-     final int vcn = student['vcn'] ?? 0;
-     final int dh = student['dh'] ?? 0;
-     final int dt = student['dt'] ?? 0;
-     final int kld = student['kld'] ?? 0;
-     final int total = work+sick+np+vcn+dh+dt+kld;
-     sheet.appendRow([name,className,present,work,sick,np,vcn,dh,dt,kld, total ]);
-    }
-    // Lưu file vào thiết bị
-    final directory = await getApplicationDocumentsDirectory();
-    final filePath = '${directory.path}/THỐNG_KÊ_ĐIỂM_DANH.xlsx';
-    final fileBytes = ex.encode();
-    final file = File(filePath);
-    await file.writeAsBytes(fileBytes!);
-    ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("File Excel đã lưu tại: $filePath"),
-          backgroundColor: Colors.red,
-          duration: const Duration(seconds: 2)),
-    );
-    await Share.shareXFiles([XFile(filePath)], text: 'Thống kê điểm danh');
-    }catch(e){
+      // Ghi dữ liệu từng cán bộ
+      for (var student in studentsData) {
+        final String name = student['name'] ?? '';
+        final String className = student['className'] ?? '';
+        final int present = student['present'] ?? 0;
+        final int work = student['work'] ?? 0;
+        final int sick = student['sick'] ?? 0;
+        final int np = student['np'] ?? 0;
+        final int vcn = student['vcn'] ?? 0;
+        final int dh = student['dh'] ?? 0;
+        final int dt = student['dt'] ?? 0;
+        final int kld = student['kld'] ?? 0;
+        final int total = work + sick + np + vcn + dh + dt + kld;
+        sheet.appendRow([
+          name,
+          className,
+          present,
+          work,
+          sick,
+          np,
+          vcn,
+          dh,
+          dt,
+          kld,
+          total,
+        ]);
+      }
+      // Lưu file vào thiết bị
+      if (kIsWeb) {
+        final bytes = ex.encode();
+        final content = base64Encode(bytes!);
+        final anchor =
+            htmt.AnchorElement(
+                href:
+                    "data:application/octet-stream;charset=utf-16le;base64,$content",
+              )
+              ..setAttribute("download", "THONG_KE_DIEM_DANH.xlsx")
+              ..click();
+      } else {
+        final directory = await getApplicationDocumentsDirectory();
+        final filePath = '${directory.path}/THỐNG_KÊ_ĐIỂM_DANH.xlsx';
+        final fileBytes = ex.encode();
+        final file = File(filePath);
+        await file.writeAsBytes(fileBytes!);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text("File Excel đã lưu tại: $filePath"),
+            backgroundColor: Colors.red,
+            duration: const Duration(seconds: 2),
+          ),
+        );
+        await Share.shareXFiles([XFile(filePath)], text: 'Thống kê điểm danh');
+      }
+    } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text("Lỗi xuất Excel: $e"),
@@ -127,8 +157,8 @@ class _StudentsStatisticsPageState extends State<StudentsStatisticsPage> {
           duration: const Duration(seconds: 2),
         ),
       );
+    }
   }
-   }
 
   @override
   Widget build(BuildContext context) {
@@ -228,7 +258,7 @@ class _StudentsStatisticsPageState extends State<StudentsStatisticsPage> {
                   children: [
                     ElevatedButton.icon(
                       onPressed: fetchFilteredData,
-                      icon: const Icon(Icons.filter_alt, color: Colors.blue,),
+                      icon: const Icon(Icons.filter_alt, color: Colors.blue),
                       label: const Text(
                         'Lọc',
                         style: TextStyle(
@@ -239,7 +269,6 @@ class _StudentsStatisticsPageState extends State<StudentsStatisticsPage> {
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.yellow,
                       ),
-
                     ),
                     const SizedBox(width: 10),
                     ElevatedButton.icon(
@@ -251,8 +280,9 @@ class _StudentsStatisticsPageState extends State<StudentsStatisticsPage> {
                           toDate = null;
                         });
                       },
-                      icon: const Icon(Icons.refresh, color: Colors.white,),
-                      label: const Text('Làm mới',
+                      icon: const Icon(Icons.refresh, color: Colors.white),
+                      label: const Text(
+                        'Làm mới',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
@@ -261,21 +291,24 @@ class _StudentsStatisticsPageState extends State<StudentsStatisticsPage> {
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.blueAccent,
                       ),
-
                     ),
                     const SizedBox(width: 10),
                     ElevatedButton.icon(
                       onPressed: () => exportToExcel(context, studentsData),
-                      icon: const Icon(Icons.download_outlined, color: Colors.white,),
-                      label: const Text('Xuất excel',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
+                      icon: const Icon(
+                        Icons.download_outlined,
                         color: Colors.white,
-                      ),),
+                      ),
+                      label: const Text(
+                        'Xuất excel',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
                       style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.green,
                       ),
-
                     ),
                   ],
                 ),
