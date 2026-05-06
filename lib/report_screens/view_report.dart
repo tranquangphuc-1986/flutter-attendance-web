91102af330dc39f673afe5f7f5123a30afab3eb6
tranquangphuc-1986 <phucthanh1572013@gmail.com>
2026-05-06 10:58:10 +0700

    vs19

--- lib/report_screens/view_report.dart
+++ lib/report_screens/view_report.dart
@@ -3,8 +3,6 @@ import 'package:app_02/service/report_firebase_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
-
 class ViewReport extends StatefulWidget {
   final DailyReport report;
   const ViewReport({super.key, required this.report});
@@ -13,12 +11,10 @@ class ViewReport extends StatefulWidget {
 }
 
 class _ViewReportState extends State<ViewReport> {
-
   // late TextEditingController titleCtrl;
   // late TextEditingController contentCtrl;
   // late TextEditingController createdAtCtrl;
   // late TextEditingController fullNameCtrl;
-
 
   @override
   void initState() {
@@ -41,74 +37,133 @@ class _ViewReportState extends State<ViewReport> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Nội dung báo cáo")),
+      appBar: AppBar(
+        title: Row(
+          children: [
+            CircleAvatar(
+              radius: 20,
+              backgroundColor: null,
+              backgroundImage: null,
+              child: Image.asset("img/logocand.png", height: 20),
+            ),
+            const Text("Nội dung báo cáo"),
+          ],
+        ),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
           child: SingleChildScrollView(
-            child:  Column(
+            child: Column(
               children: [
                 SizedBox(height: 30),
-                Row(crossAxisAlignment: CrossAxisAlignment.center,
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                  Text("Kính gửi: Lãnh đạo Công an tỉnh", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                  SizedBox(width: 16),
-                ],
+                    Text(
+                      "Kính gửi: Lãnh đạo Công an tỉnh",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                  ],
                 ),
                 SizedBox(height: 16),
 
                 //Tiêu đề
-                Row(crossAxisAlignment: CrossAxisAlignment.center,
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                  Text("${widget.report.title}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.blue),),
-                  SizedBox(width: 16),
-                ],
+                    Text(
+                      "${widget.report.title}",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                  ],
                 ),
                 SizedBox(height: 16),
 
                 //Nội dung
-                Row(crossAxisAlignment: CrossAxisAlignment.center,
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                  Text("${widget.report.content}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                  SizedBox(width: 16),
-                ],
+                    Text(
+                      "${widget.report.content}",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                  ],
                 ),
                 SizedBox(height: 16),
 
                 //Cán bộ báo cáo
-                Row(crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                  Text("Cán bộ báo cáo: ${widget.report.fullName}", style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
-                  SizedBox(width: 16),
-                ],
-                ),
-                SizedBox(height: 8),
-
-                Row(crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                  Text("Số: ${widget.report.id}", style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
-                  SizedBox(width: 16),
-                ],
-                ),
-                SizedBox(height: 8),
-
-                Row(crossAxisAlignment: CrossAxisAlignment.end,
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                  Text("Ngày tạo: ${widget.report.createdAt.toDate().day}/${widget.report.createdAt.toDate().month}/${widget.report.createdAt.toDate().year}", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-                  SizedBox(width: 16),
-                ],
+                    Text(
+                      "Cán bộ báo cáo: ${widget.report.fullName}",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                  ],
+                ),
+                SizedBox(height: 4),
+
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Số: ${widget.report.id}",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                  ],
+                ),
+                SizedBox(height: 4),
+
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Ngày tạo: ${widget.report.createdAt.toDate().day}/${widget.report.createdAt.toDate().month}/${widget.report.createdAt.toDate().year}",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                  ],
                 ),
 
                 const SizedBox(height: 50),
                 ElevatedButton(
                   onPressed: () => Navigator.pop(context),
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                  child: const Text(
+                    'Đóng',
+                    style: TextStyle(color: Colors.white),
                   ),
-                  child: const Text('Đóng', style: TextStyle(color: Colors.white,)),
                 ),
               ],
             ),
