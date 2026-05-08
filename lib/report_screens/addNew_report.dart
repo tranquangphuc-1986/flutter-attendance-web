4eecb3d07f2ec280633c671544b7a7ab60d70481
tranquangphuc-1986 <phucthanh1572013@gmail.com>
2026-05-08 15:27:05 +0700

    Auto update and deploy Fri 05/08/2026 15:27:04.97

--- lib/report_screens/addNew_report.dart
+++ lib/report_screens/addNew_report.dart
@@ -117,7 +117,7 @@ class _AddnewReportState extends State<AddnewReport> {
           icon: Icon(Icons.arrow_back_ios_new, color: Colors.white),
           onPressed: () => Navigator.pop(context),
         ),
-        backgroundColor: Colors.cyan,
+        backgroundColor: Colors.blue,
         title: const Text("Thêm mới báo cáo", style: TextStyle(color: Colors.white),),
         centerTitle: true,
       ),
